6b08de4bdf796c5d2ee03a245be12aec29e23e71
Janusha-Weerasinghe <amjanushachamali@gmail.com>
2025-01-10 20:56:50 +0530

    When book booking ,mail send the user email addreess including the booking details

--- server/middlewares/sendMail.js
+++ server/middlewares/sendMail.js
@@ -1,11 +1,11 @@
-const nodemailer = require ('nodemailer')
+const nodemailer = require('nodemailer');
 
-const transtport = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
-        pass: process.env.CODE_SENDING_EMAIL_PASSWORD,
-    }
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
+    pass: process.env.CODE_SENDING_EMAIL_PASSWORD,
+  }
 });
 
-module.exports=transtport;
+module.exports = transporter;
